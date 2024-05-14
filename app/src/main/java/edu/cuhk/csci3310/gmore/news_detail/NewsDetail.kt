4c0b0a4510eb46d17734ea565d3c9a7ce00f125c
jimmylau <jimmylaukh@gmail.com>
2024-05-14 13:31:20 +0800

    feat: detail layout update

--- app/src/main/java/edu/cuhk/csci3310/gmore/news_detail/NewsDetail.kt
+++ app/src/main/java/edu/cuhk/csci3310/gmore/news_detail/NewsDetail.kt
@@ -1,7 +1,9 @@
 package edu.cuhk.csci3310.gmore.news_detail
 
 import android.net.Uri
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -20,8 +22,10 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.font.FontWeight
@@ -33,6 +37,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import edu.cuhk.csci3310.gmore.data.db.MarkedNews
+import edu.cuhk.csci3310.gmore.ui.theme.OffWhite
 import edu.cuhk.csci3310.gmore.util.UiEvent
 
 
@@ -57,74 +62,118 @@ fun NewsDetailScreen(
 
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
     val imagePainter = rememberAsyncImagePainter(model = Uri.parse(viewModel.imageUrl))
-    Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(OffWhite),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
 
-        topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text(
-                        text = "Detail",
-                        fontSize = 16.sp,
-                        textAlign = TextAlign.Left,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                },
-                navigationIcon = {
-                    // TODO: Pop backstack
-                    IconButton(onClick = {
-                        viewModel.onEvent(NewsDetailEvent.onBackClick)
-                    }) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "Localized description"
+            topBar = {
+                TopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    title = {
+                        Text(
+                            text = "Detail",
+                            fontSize = 16.sp,
+                            textAlign = TextAlign.Left,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
                         )
-                    }
-                },
-                actions = {
-                    // TODO: add to bookmark
-                    val markedNews = MarkedNews(
-                        id = viewModel.id,
-                        title = viewModel.title,
-                        imageUrl = viewModel.imageUrl,
-                        source = viewModel.source,
-                        sourceUrl = viewModel.sourceUrl,
-                        publishedDate = viewModel.publishedDate,
-                        summary1 = viewModel.point1,
-                        summary2 = viewModel.point2,
-                        summary3 = viewModel.point3
-                    )
-                    CustomCheckbox(
-                        checked = markedNews.isSaved,
-                        onCheckedChange = { isChecked ->
-                            viewModel.onEvent(NewsDetailEvent.onSaveNewsClick(markedNews, isChecked))
+                    },
+                    navigationIcon = {
+                        // TODO: Pop backstack
+                        IconButton(onClick = {
+                            viewModel.onEvent(NewsDetailEvent.onBackClick)
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.ArrowBack,
+                                contentDescription = "Localized description"
+                            )
                         }
-                    )
-                },
-                scrollBehavior = scrollBehavior,
-            )
-        },
-    ) { innerPadding ->
-        when(uiState)  {
-            NewsDetailUiState.Loading -> {
-                Text("Loading")
-            }
-            NewsDetailUiState.Error -> {
-                Text("Error")
-            }
-            NewsDetailUiState.Success -> {
-                NewsContent(innerPadding, viewModel, imagePainter)
-            }
-            NewsDetailUiState.Empty -> {
-                Text("Empty")
-            }
+                    },
+                    actions = {
+                        // TODO: add to bookmark
+                        val markedNews = MarkedNews(
+                            id = viewModel.id,
+                            title = viewModel.title,
+                            imageUrl = viewModel.imageUrl,
+                            source = viewModel.source,
+                            sourceUrl = viewModel.sourceUrl,
+                            publishedDate = viewModel.publishedDate,
+                            summary1 = viewModel.point1,
+                            summary2 = viewModel.point2,
+                            summary3 = viewModel.point3
+                        )
+                        CustomCheckbox(
+                            checked = markedNews.isSaved,
+                            onCheckedChange = { isChecked ->
+                                viewModel.onEvent(
+                                    NewsDetailEvent.onSaveNewsClick(
+                                        markedNews,
+                                        isChecked
+                                    )
+                                )
+                            }
+                        )
+                    },
+                    scrollBehavior = scrollBehavior,
+                )
+            },
+        ) { innerPadding ->
+            when (uiState) {
+                NewsDetailUiState.Loading -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(OffWhite),
+                        contentAlignment = Alignment.TopCenter
+                    ) {
+                        Text("Loading")
+                    }
+                }
 
-            else -> {}
+                NewsDetailUiState.Error -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(OffWhite),
+                        contentAlignment = Alignment.TopCenter
+                    ) {
+                        Text("Error")
+                    }
+                }
+
+                NewsDetailUiState.Success -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(OffWhite),
+                        contentAlignment = Alignment.TopCenter
+                    ) {
+                        Spacer(modifier = Modifier.height(16.dp))
+                        NewsContent(innerPadding, viewModel, imagePainter)
+                    }
+                }
+
+                NewsDetailUiState.Empty -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(OffWhite),
+                        contentAlignment = Alignment.TopCenter
+                    ) {
+                        Text("Empty")
+                    }
+                }
+
+                else -> {}
+            }
         }
     }
 }
@@ -148,38 +197,120 @@ fun NewsContent(
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(
-            text = "News Summary",
-            style = MaterialTheme.typography.bodySmall,
-            fontWeight = FontWeight.Bold
+            text = "★Smart View",
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = Color.Black,
+            fontSize = MaterialTheme.typography.titleSmall.fontSize,
+            fontWeight = MaterialTheme.typography.titleSmall.fontWeight,
         )
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        Text(
-            text = viewModel.point1,
-            style = MaterialTheme.typography.bodySmall
-        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Spacer(
+                Modifier
+                    .size(5.dp)
+                    .fillMaxHeight()
+                    .background(Color.Transparent)
+            )
+            Canvas(
+                modifier = Modifier
+                    .size(10.dp)
+            ) {
+                drawCircle(Color.Black)
+            }
 
-        Text(
-            text = viewModel.point2,
-            style = MaterialTheme.typography.bodySmall
-        )
+            Spacer(
+                Modifier
+                    .size(10.dp)
+                    .fillMaxHeight()
+                    .background(Color.Transparent)
+            )
 
-        Text(
-            text = viewModel.point3,
-            style = MaterialTheme.typography.bodySmall
-        )
+            Text(
+                text = viewModel.point1,
+                color = Color.Black,
+                lineHeight = MaterialTheme.typography.bodySmall.lineHeight
+            )
+        }
+
+        Spacer(modifier = Modifier
+            .padding(vertical = 3.dp)
+            .background(Color.Transparent))
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Spacer(
+                Modifier
+                    .size(5.dp)
+                    .fillMaxHeight()
+                    .background(Color.Transparent)
+            )
+            Canvas(
+                modifier = Modifier
+                    .size(10.dp)
+            ) {
+                drawCircle(Color.Black)
+            }
+
+            Spacer(
+                Modifier
+                    .size(10.dp)
+                    .fillMaxHeight()
+                    .background(Color.Transparent)
+            )
+
+            Text(
+                text = viewModel.point2,
+                color = Color.Black,
+                lineHeight = MaterialTheme.typography.bodySmall.lineHeight
+            )
+        }
+
+        Spacer(modifier = Modifier
+            .padding(vertical = 3.dp)
+            .background(Color.Transparent))
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Spacer(
+                Modifier
+                    .size(5.dp)
+                    .fillMaxHeight()
+                    .background(Color.Transparent)
+            )
+            Canvas(
+                modifier = Modifier
+                    .size(10.dp)
+            ) {
+                drawCircle(Color.Black)
+            }
+
+            Spacer(
+                Modifier
+                    .size(10.dp)
+                    .fillMaxHeight()
+                    .background(Color.Transparent)
+            )
+
+            Text(
+                text = viewModel.point3,
+                color = Color.Black,
+                lineHeight = MaterialTheme.typography.bodySmall.lineHeight
+            )
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(
             text = viewModel.source,
-            style = MaterialTheme.typography.bodySmall
+            style = MaterialTheme.typography.bodySmall,
+            color = Color.Black
         )
 
         Text(
             text = viewModel.publishedDate,
-            style = MaterialTheme.typography.bodySmall
+            style = MaterialTheme.typography.bodySmall,
+            color = Color.Black
         )
 
         Spacer(modifier = Modifier.height(16.dp))
